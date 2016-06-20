8d435c616da81b8d646720683fda7353a9399b3a
lanouhn <2462650365@qq.com>
2016-06-20 16:47:02 +0800

    添加FristTableViewController

---
+++ GitTest/GitTest/FristTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FristTableViewController.h
+//  GitTest
+//
+//  Created by lanouhn on 16/6/20.
+//  Copyright © 2016年 lanouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FristTableViewController : UITableViewController
+
+@end
